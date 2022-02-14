2c93524b806940c3af51aac03f8d1b2d0ba89765
Steve S. Samson <stevee.samson@gmail.com>
2022-02-14 13:23:38 +0100

    add formating to population

--- client/src/components/Details/HomePlanet.js
+++ client/src/components/Details/HomePlanet.js
@@ -1,3 +1,13 @@
+export const formatCount = (num) => {
+  return num > 1000000000
+    ? `${(num / 1000000000).toFixed(0)} Billion`
+    : num > 1000000
+    ? `${(num / 1000000).toFixed(0)} Million`
+    : num > 1000
+    ? `${(num / 1000).toFixed(0)} Thousand`
+    : num;
+};
+
 const HomePlanet = ({ title, terrain, population }) => (
   <table>
     <tbody>
@@ -11,7 +21,7 @@ const HomePlanet = ({ title, terrain, population }) => (
       </tr>
       <tr>
         <th>Population</th>
-        <td>{population}</td>
+        <td>{formatCount(population)}</td>
       </tr>
     </tbody>
   </table>
